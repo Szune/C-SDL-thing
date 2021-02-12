a67176173dfacddf79c03d1f0959b2f6b9a656d9
Szune <erikiwarson@gmail.com>
2021-02-12 01:09:28 +0100

    add colorful sprites, map, misc

--- main.c
+++ main.c
@@ -5,32 +5,88 @@
 #include "option.h"
 #include "result.h"
 #include "list.h"
+#include "map.h"
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 #define SPRITE_SIZE 32
 #define SMALL_SPRITE_SIZE 16
+#define TINY_SPRITE_SIZE 8
+#define TINY_SPRITE_SCALE 4
+
+typedef struct {
+	u8 r;
+	u8 g;
+	u8 b;
+	u8 a;
+} RgbaColor;
+
+#define RGB(rv,gv,bv) {.r = (rv), .g = (gv), .b = (bv), .a = 255 }
+
 
 /* sprites */
-// 16x16 sprite
-const bool g_mouse_sprite[256] = {
-1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,
-0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,
-0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,
-0,0,1,1,1,1,1,1,1,1,1,0,0,1,0,0,
-0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,
-0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,
-0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,
-0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,
-0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,
-0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,
-0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,
-0,0,0,0,0,1,1,1,1,0,1,0,1,0,0,0,
-0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,
-0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
+// 16x16 sprites
+const u8 g_mouse_sprite_1byte[256] = {
+  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  1,255,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  1,255,255,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  1,255,255,255,255,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  1,255,255,255,255,  1,  1,  1,  1,  0,  0,  1,  0,  0,
+  0,  0,  1,255,255,255,255,255,255,255,255,  1,  1,  0,  0,  0,
+  0,  0,  0,  1,255,255,255,255,255,255,255,255,  1,  0,  0,  0,
+  0,  0,  0,  0,  1,255,255,255,255,255,255,255,  1,  0,  0,  0,
+  0,  0,  0,  0,  1,255,255,255,255,255,255,255,  1,  0,  0,  0,
+  0,  0,  0,  0,  1,255,255,255,255,255,255,  1,  0,  0,  0,  0,
+  0,  0,  0,  0,  1,255,255,255,255,255,255,  1,  1,  0,  0,  0,
+  0,  0,  0,  0,  0,  1,255,255,255,  1,  1,  1,  1,  0,  0,  0,
+  0,  0,  0,  0,  0,  1,  1,  1,  1,  0,  1,  1,255,  1,  0,  0,
+  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  0,  1,255,  1,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,255,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1
 };
+
+const u8 g_creature_sprite_1byte[256] = {
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  1,255,255,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  1,255,255,255,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  1,  1,255,255,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  1,255,255,255,255,  1,  1,  1,  1,  0,  0,  0,  0,  0,
+  0,  0,  1,255,255,255,255,255,255,255,  1,  1,  0,  0,  0,  0,
+  0,  0,  0,  1,255,255,255,255,255,255,255,  1,  0,  0,  0,  0,
+  0,  0,  0,  1,255,255,255,255,255,255,255,255,  1,  0,  0,  0,
+  0,  0,  1,  1,  1,255,  1,  1,  1,255,255,255,255,  1,  1,  0,
+  0,  1,255,  1,  1,  1,  1,  0,  1,255,  1,255,255,255,255,  1,
+  0,  1,  1,  0,  0,  0,  0,  0,  1,255,  1,  1,  1,255,  1,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,255,  1,  1,  1,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,255,  1,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+};
+
+// 8x8 sprites
+const RgbaColor g_grass_tile_rgba[64] = {
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),RGB(120,255,100),
+};
+
+const RgbaColor g_water_tile_rgba[64] = {
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),RGB(120,100,255),
+};
+
 /* sprites */
 
 // \/ from https://wiki.libsdl.org/SDL_CreateRGBSurface
@@ -50,7 +106,28 @@ const Uint32 g_amask = 0xff000000;
 #endif
 // /\ from https://wiki.libsdl.org/SDL_CreateRGBSurface
 
-SDL_Texture* CreateSmallTexture(SDL_Renderer* renderer, const bool sprite[256]) {
+SDL_Texture* CreateTinyTexture4Byte(SDL_Renderer* renderer, const RgbaColor sprite[64]) {
+
+	// TODO: get size from sprite info?
+	SDL_Surface* temp = SDL_CreateRGBSurface(0, TINY_SPRITE_SIZE, TINY_SPRITE_SIZE, 32, g_rmask, g_gmask, g_bmask, g_amask);
+
+
+	int len = temp->h * temp->pitch;
+	int i;
+	int c;
+	for (i = 0, c = 0; i < len; i+=4, ++c) {
+		*((Uint8*)temp->pixels + i) = sprite[c].r; // R
+		*((Uint8*)temp->pixels + i + 1) = sprite[c].g; // G
+		*((Uint8*)temp->pixels + i + 2) = sprite[c].b; // B
+		*((Uint8*)temp->pixels + i + 3) = sprite[c].a; // A
+	}
+	
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, temp);
+	SDL_FreeSurface(temp);
+	return tex;
+}
+
+SDL_Texture* CreateSmallTexture1Byte(SDL_Renderer* renderer, const u8 sprite[256]) {
 
 	// TODO: get size from sprite info?
 	SDL_Surface* temp = SDL_CreateRGBSurface(0, SMALL_SPRITE_SIZE, SMALL_SPRITE_SIZE, 32, g_rmask, g_gmask, g_bmask, g_amask);
@@ -60,11 +137,11 @@ SDL_Texture* CreateSmallTexture(SDL_Renderer* renderer, const bool sprite[256]) 
 	int i;
 	int c;
 	for (i = 0, c = 0; i < len; i+=4, ++c) {
-		if (sprite[c]) {
-			*((Uint8*)temp->pixels + i) = 255;
-			*((Uint8*)temp->pixels + i + 1) = 255;
-			*((Uint8*)temp->pixels + i + 2) = 255;
-			*((Uint8*)temp->pixels + i + 3) = 255;
+		if (sprite[c]) { // if sprite[c] > 0
+			*((Uint8*)temp->pixels + i) = sprite[c]; // R
+			*((Uint8*)temp->pixels + i + 1) = sprite[c]; // G
+			*((Uint8*)temp->pixels + i + 2) = sprite[c]; // B
+			*((Uint8*)temp->pixels + i + 3) = 255; // A
 		}
 	}
 	
@@ -109,8 +186,11 @@ int main(int argc, char *argv[]) {
 
 	// setup palette
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_Texture* tex_mouse = CreateSmallTexture(renderer, g_mouse_sprite);
+	SDL_Texture* tex_mouse = CreateSmallTexture1Byte(renderer, g_mouse_sprite_1byte);
 	SDL_Texture* texture = CreateTexture(renderer);
+	SDL_Texture* tex_grass_tile = CreateTinyTexture4Byte(renderer, g_grass_tile_rgba);
+	SDL_Texture* tex_water_tile = CreateTinyTexture4Byte(renderer, g_water_tile_rgba);
+	SDL_Texture* tex_creature = CreateSmallTexture1Byte(renderer, g_creature_sprite_1byte);
 
 	SDL_Surface* screen = SDL_GetWindowSurface(window);
 	SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0x33, 0x33, 0x33)); // set background to #333
@@ -119,17 +199,39 @@ int main(int argc, char *argv[]) {
 	SDL_Event event;
 	bool gameover = false;
 
+	/*
 	SDL_Rect tex_draw_r = { 
 		.x = SCREEN_WIDTH / 2,
 		.y = SCREEN_HEIGHT / 2,
 		.w = SPRITE_SIZE * 2,
 		.h = SPRITE_SIZE * 2 };
+	*/
+
+
+	SDL_Rect tex_map_tile_r = { 
+		.x = 0,
+		.y = 0,
+		.w = TINY_SPRITE_SIZE * TINY_SPRITE_SCALE,
+		.h = TINY_SPRITE_SIZE * TINY_SPRITE_SCALE };
+
 
 	SDL_Rect tex_mouse_draw_r = { 
 		.x = (SCREEN_WIDTH / 2) + 16 + (SPRITE_SIZE * 2),
 		.y = (SCREEN_HEIGHT / 2) + 16+ (SPRITE_SIZE * 2),
 		.w = SMALL_SPRITE_SIZE,
 		.h = SMALL_SPRITE_SIZE};
+
+	SDL_Rect tex_creature_draw_r = { 
+		.x = (SCREEN_WIDTH / 2) - (SPRITE_SIZE * 2),
+		.y = (SCREEN_HEIGHT / 2) - (SPRITE_SIZE * 2),
+		.w = SMALL_SPRITE_SIZE,
+		.h = SMALL_SPRITE_SIZE};
+
+	u16 max_x = SCREEN_WIDTH / (TINY_SPRITE_SIZE * TINY_SPRITE_SCALE);
+	u16 max_y = SCREEN_HEIGHT / (TINY_SPRITE_SIZE * TINY_SPRITE_SCALE);
+
+	u8 creature_state = 0;
+
 
 	while(!gameover) {
 		if(SDL_PollEvent(&event)) {
@@ -140,7 +242,7 @@ int main(int argc, char *argv[]) {
 				case SDL_KEYDOWN:
 					switch(event.key.keysym.sym) {
 						case SDLK_ESCAPE:
-						//case SDLK_q:
+						case SDLK_q:
 							gameover = true;
 							break;
 					}
@@ -161,7 +263,49 @@ int main(int argc, char *argv[]) {
 		// [rects are of type SDL_Rect*]
 		// src rect == NULL means drawing the entire texture
 		// dst rect == NULL means drawing the texture over the entire rendering target
-		SDL_RenderCopy(renderer, texture, NULL, &tex_draw_r);
+		//SDL_RenderCopy(renderer, texture, NULL, &tex_draw_r);
+
+		// draw map (defined in map.h)
+		for(u16 y = 0; y < max_y; ++y) {
+			for(u16 x = 0; x < max_x; ++x) {
+				tex_map_tile_r.x = TINY_SPRITE_SIZE * TINY_SPRITE_SCALE * x;
+				tex_map_tile_r.y = TINY_SPRITE_SIZE * TINY_SPRITE_SCALE * y;
+				switch(g_game_map[(y * max_x) + x]) {
+					case 1:
+						SDL_RenderCopy(renderer, tex_grass_tile, NULL, &tex_map_tile_r);
+						break;
+					case 2:
+						SDL_RenderCopy(renderer, tex_water_tile, NULL, &tex_map_tile_r);
+						break;
+				}
+			}
+		}
+
+		// draw creature
+		SDL_RenderCopy(renderer, tex_creature, NULL, &tex_creature_draw_r);
+		/* TODO: implement game time to keep track of timers
+		switch(creature_state) {
+			case 0:
+				SDL_RenderCopy(renderer, tex_creature, NULL, &tex_creature_draw_r);
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+		}
+		*/
+
+		/*
+		SDL_RenderCopy(renderer, tex_grass_tile, NULL, &tex_grass_tile_r);
+		tex_grass_tile_r.x += TINY_SPRITE_SIZE * TINY_SPRITE_SCALE;
+		SDL_RenderCopy(renderer, tex_grass_tile, NULL, &tex_grass_tile_r);
+		tex_grass_tile_r.x -= TINY_SPRITE_SIZE * TINY_SPRITE_SCALE;
+		*/
+
+
+		// draw mouse over everything else
 		SDL_RenderCopy(renderer, tex_mouse, NULL, &tex_mouse_draw_r);
 		// ---
 
@@ -174,6 +318,9 @@ int main(int argc, char *argv[]) {
 	// destroy all textures
 	SDL_DestroyTexture(tex_mouse);
 	SDL_DestroyTexture(texture);
+	SDL_DestroyTexture(tex_grass_tile);
+	SDL_DestroyTexture(tex_water_tile);
+	SDL_DestroyTexture(tex_creature);
 
 	// destroy renderer
 	SDL_DestroyRenderer(renderer);
